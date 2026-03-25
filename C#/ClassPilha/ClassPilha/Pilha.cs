71e7c483e20443e4e2a6252003700e2f7c7ddeac
Marcos de Castro Lima Souza <marcosclsou@gmail.com>
2026-03-25 18:54:54 -0300

    Add files via upload

---
+++ C#/ClassPilha/ClassPilha/Pilha.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPilha
+{
+    internal class Pilha
+    {
+        private int[] vetor;
+        private int topo;
+
+        public Pilha(int capacidade)
+        {
+            vetor = new int[capacidade];
+        }
+
+        public bool vazio()
+        {
+            return topo == 0;
+        }
+        public bool cheia()
+        {
+            return topo == vetor.Length;
+        }
+
+        public void empilhar(int valor)
+        {
+            if (!cheia())
+            {
+                vetor[topo] = valor;
+                topo++;
+            }
+        }
+        public int desempilhar()
+        {
+            if (!vazio())
+            {
+                topo--;
+                return vetor[topo];
+            }
+            return 0;
+        }
+        public void imprimir()
+        {
+            string itens = "";
+
+            for (int i = 0; i < topo; i++)
+            {
+                itens += $"{vetor[i]}, ";
+                Console.WriteLine($"[ {(itens.Length == 0 ? itens : itens.Substring(0, itens.Length - 2))} ]");
+
+
+            }
+
+        }
+    }
+}
---
+++ C#/ClassPilha/ClassPilha/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = { 1,2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Pilha par = new Pilha ( vet.Length / 2 );
+            Pilha impar = new Pilha ( vet.Length / 2 );
+
+
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    par.empilhar(vet[i]);
+
+                
+                }
+                else
+                {
+                    impar.empilhar(vet[i]);
+                }
+            }
+            for(int i = 0; i<vet.Length/2; i++)
+            {
+                int p = par.desempilhar();
+                int ip = impar.desempilhar();
+
+                vet[i * 2] = p;
+                vet[i * 2 + 1] = ip;
+            }
+            for(int i = 0; i< vet.Length; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+            Console.ReadKey();
+            
+
+
+
+        }
+
+       
+
+        }
+    }
+ 
